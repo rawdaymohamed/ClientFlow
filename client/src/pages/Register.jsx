bef98d73bd1d46c566fd79fa036aeff94651f9b4
Rawda Yasser <rawdaymohamed@gmail.com>
2026-04-15 20:59:00 +0200

    Feat: add zod validation to register

--- client/src/pages/Register.jsx
+++ client/src/pages/Register.jsx
@@ -1,26 +1,55 @@
+import { zodResolver } from "@hookform/resolvers/zod";
+import { useForm } from "react-hook-form";
+import { registerSchema } from "../features/auth/auth.schema";
 const Register = () => {
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm({
+    resolver: zodResolver(registerSchema),
+  });
+
+  const onSubmit = (data) => {
+    console.log(data);
+  };
+
   return (
     <div className="min-h-screen flex items-center justify-center bg-gray-100">
       <div className="bg-white p-6 rounded-lg shadow-md w-full max-w-md">
         <h1 className="text-2xl font-bold mb-4 text-center">Register</h1>
 
-        <form className="space-y-4">
+        <form
+          onSubmit={handleSubmit(onSubmit)}
+          className="space-y-4"
+          noValidate
+        >
           <div>
             <label className="block mb-1 text-sm font-medium">Name</label>
             <input
               type="text"
               placeholder="Enter your name"
+              {...register("name")}
               className="w-full rounded-md border border-gray-300 px-3 py-2 outline-none focus:ring-2 focus:ring-gray-400"
             />
+            {errors.name && (
+              <p className="text-red-500 text-sm mt-1">{errors.name.message}</p>
+            )}
           </div>
 
           <div>
             <label className="block mb-1 text-sm font-medium">Email</label>
             <input
               type="email"
+              {...register("email")}
               placeholder="Enter your email"
               className="w-full rounded-md border border-gray-300 px-3 py-2 outline-none focus:ring-2 focus:ring-gray-400"
             />
+            {errors.email && (
+              <p className="text-red-500 text-sm mt-1">
+                {errors.email.message}
+              </p>
+            )}
           </div>
 
           <div>
@@ -28,8 +57,14 @@ const Register = () => {
             <input
               type="password"
               placeholder="Create a password"
+              {...register("password")}
               className="w-full rounded-md border border-gray-300 px-3 py-2 outline-none focus:ring-2 focus:ring-gray-400"
             />
+            {errors.password && (
+              <p className="text-red-500 text-sm mt-1">
+                {errors.password.message}
+              </p>
+            )}
           </div>
 
           <button
