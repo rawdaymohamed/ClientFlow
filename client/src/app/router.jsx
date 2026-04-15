b4d5d4a22aa76d827123afd91d2414045cdadcd2
Rawda Yasser <rawdaymohamed@gmail.com>
2026-04-15 17:42:05 +0200

    feat: connect routes to page components

--- client/src/app/router.jsx
+++ client/src/app/router.jsx
@@ -1,4 +1,7 @@
 import { createBrowserRouter } from "react-router-dom";
+import Login from "../pages/Login";
+import Register from "../pages/Register";
+import Dashboard from "../pages/Dashboard";
 
 const router = createBrowserRouter([
   {
@@ -7,15 +10,15 @@ const router = createBrowserRouter([
   },
   {
     path: "/login",
-    element: <div>Login Page</div>,
+    element: <Login />,
   },
   {
     path: "/register",
-    element: <div>Register Page</div>,
+    element: <Register />,
   },
   {
     path: "/dashboard",
-    element: <div>Dashboard Page</div>,
+    element: <Dashboard />,
   },
 ]);
 
