0aeadfd2905f6b9a7cc5a387b00c458bcb9d9eef
Rawda Yasser <rawdaymohamed@gmail.com>
2026-04-15 20:27:34 +0200

    chore: install react-hook-form zod tailwind css dependencies

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -4,6 +4,7 @@ import { RouterProvider } from "react-router-dom";
 import { QueryClientProvider } from "@tanstack/react-query";
 import router from "./app/router.jsx";
 import queryClient from "./app/queryClient.js";
+import "./index.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
