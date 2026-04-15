5a2c99c28969882d7b64ddb506efa65142acf8a2
Rawda Yasser <rawdaymohamed@gmail.com>
2026-04-16 00:44:37 +0200

    fix: fix connectdb on vercel

--- server/src/server.js
+++ server/src/server.js
@@ -5,17 +5,13 @@ import connectDB from "./config/db.js";
 dotenv.config();
 const PORT = process.env.PORT || 5000;
 
-const startServer = async () => {
-  try {
-    await connectDB();
+connectDB();
 
-    app.listen(PORT, () => {
-      console.log(`🚀 Server is running on port ${PORT}`);
-    });
-  } catch (error) {
-    console.error("❌ Failed to start server:", error.message);
-    process.exit(1);
-  }
-};
+if (process.env.NODE_ENV !== "production") {
+  const PORT = process.env.PORT || 5000;
+  app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`);
+  });
+}
 
-startServer();
+export default app;
