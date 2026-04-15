76fe9bc755cdaa3f0089e4ad0f8645e54c187f4e
Rawda Yasser <rawdaymohamed@gmail.com>
2026-04-15 20:41:15 +0200

    feat: add login form zod schema

---
+++ client/src/features/auth/auth.schema.js
@@ -0,0 +1,5 @@
+import { z } from "zod";
+export const loginSchema = z.object({
+  email: z.string().trim().toLowerCase().email("Invalid email address"),
+  password: z.string().min(1, "Password is required"),
+});
