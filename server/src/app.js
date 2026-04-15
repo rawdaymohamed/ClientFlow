6e2fc9fb80fbb993b475a8ac3f8a7d96723e3f20
Rawda Yasser <rawdaymohamed@gmail.com>
2026-04-15 23:29:03 +0200

    fix: fix vercel cors issue

--- server/src/app.js
+++ server/src/app.js
@@ -16,6 +16,7 @@ app.use(
   cors({
     origin: process.env.CLIENT_URL,
     credentials: true,
+    methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"], // Add this
   }),
 );
 app.use(morgan("dev"));
