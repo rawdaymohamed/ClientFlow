f55384d2bf97771f094958858614bcc6a57e38da
Rawda Yasser <rawdaymohamed@gmail.com>
2026-04-16 00:51:55 +0200

    fix: fix vercel api deployment

--- server/src/server.js
+++ server/src/server.js
@@ -1,11 +1,49 @@
+import express from "express";
+import cors from "cors";
+import morgan from "morgan";
+import helmet from "helmet";
+import cookieParser from "cookie-parser";
+import authRoutes from "./routes/auth.routes.js";
 import dotenv from "dotenv";
-import app from "./app.js";
 import connectDB from "./config/db.js";
 
 dotenv.config();
-const PORT = process.env.PORT || 5000;
 
 connectDB();
+
+const app = express();
+
+app.use(express.json());
+app.use(cookieParser());
+app.use(morgan("dev"));
+app.use(helmet());
+app.use(
+  cors({
+    origin: process.env.CLIENT_URL,
+    credentials: true,
+    methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"], // Add this
+  }),
+);
+
+app.get("/api/health", (req, res) => {
+  res.status(200).json({
+    status: "success",
+    message: "API is running",
+  });
+});
+
+app.use("/api/auth", authRoutes);
+
+app.use((err, req, res, next) => {
+  if (err instanceof SyntaxError && err.status === 400 && "body" in err) {
+    return res.status(400).json({
+      status: "error",
+      message: "Invalid JSON format",
+    });
+  }
+
+  next(err);
+});
 
 if (process.env.NODE_ENV !== "production") {
   const PORT = process.env.PORT || 5000;
